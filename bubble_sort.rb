68eb32212eab5ff66f2d901dcbd5f63952d2822d
smysak <98991422+smysak@users.noreply.github.com>
2026-06-05 10:38:55 +0800

    write a method that makes 1 pass of a bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,13 @@
-array = [9, 5, 8, 6, 3, 2, 7, 1, 4]
+array_to_sort = [9, 5, 8, 6, 3, 2, 7, 1, 4]
+
+def bubble_sort(array)
+  array_length = array.size - 1
+  array_length.times do |i|
+    if array[i] > array[i.succ]
+      array[i], array[i.succ] = array[i.succ], array[i]
+      p array
+    end
+  end
+end
+
+bubble_sort(array_to_sort)
