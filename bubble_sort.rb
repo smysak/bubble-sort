921b16298569548b4aa41164e93cabcfeac7adc0
smysak <98991422+smysak@users.noreply.github.com>
2026-06-05 13:52:43 +0800

    subtract pass from the check times to optimize (stops one step early each pass since the last digit has been locked)

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,7 @@ array_to_sort = [9, 5, 8, 6, 3, 2, 7, 1, 4]
 
 def bubble_sort(array)
   array.size.times do |pass|
-    (array.size - 1).times do |i|
+    ((array.size - 1) - pass).times do |i|
       if array[i] > array[i.succ]
         array[i], array[i.succ] = array[i.succ], array[i]
         p array
