f98ba8521956eaaa3f263a03298f90653791705a
smysak <98991422+smysak@users.noreply.github.com>
2026-06-05 13:46:24 +0800

    fix the method to pass over 9 times instead of 8

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,8 @@
 array_to_sort = [9, 5, 8, 6, 3, 2, 7, 1, 4]
 
 def bubble_sort(array)
-  array_length = array.size - 1
-  array_length.times do |pass|
-    array_length.times do |i|
+  array.size.times do |pass|
+    (array.size - 1).times do |i|
       if array[i] > array[i.succ]
         array[i], array[i.succ] = array[i.succ], array[i]
         p array
