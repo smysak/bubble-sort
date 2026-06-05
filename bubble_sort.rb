c061ca595676988b8d60593f7bb3837a32529c72
smysak <98991422+smysak@users.noreply.github.com>
2026-06-05 14:05:52 +0800

    rename method bubble_sort! (since destructive)

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,6 @@
 array_to_sort = [9, 5, 8, 6, 3, 2, 7, 1, 4]
 
-def bubble_sort(array)
+def bubble_sort!(array)
   array.size.times do |pass|
     ((array.size - 1) - pass).times do |i|
       if array[i] > array[i.succ]
@@ -12,4 +12,4 @@ def bubble_sort(array)
   return array
 end
 
-bubble_sort(array_to_sort)
+bubble_sort!(array_to_sort)
