e2e53aaee4f5233aab27854db51e19cde4a626f2
smysak <98991422+smysak@users.noreply.github.com>
2026-06-05 13:36:15 +0800

    make the bubble_sort pass though the whole array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,10 +2,12 @@ array_to_sort = [9, 5, 8, 6, 3, 2, 7, 1, 4]
 
 def bubble_sort(array)
   array_length = array.size - 1
-  array_length.times do |i|
-    if array[i] > array[i.succ]
-      array[i], array[i.succ] = array[i.succ], array[i]
-      p array
+  array_length.times do |pass|
+    array_length.times do |i|
+      if array[i] > array[i.succ]
+        array[i], array[i.succ] = array[i.succ], array[i]
+        p array
+      end
     end
   end
 end
